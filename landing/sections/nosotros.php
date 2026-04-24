3ffc31b5256c48341c0f6a97e2f0946c902c5102
TwoNote-crypto <yeisonfabiandiazolaya@gmail.com>
2026-04-23 23:44:25 -0500

    login funcionando otra vez

---
+++ landing/sections/nosotros.php
@@ -0,0 +1,38 @@
+<section class="nosotros" id="nosotros">
+    <div class="container nosotros-grid">
+
+        <div class="nosotros-text">
+            <p class="tag">¿QUIÉNES SOMOS?</p>
+            <h2>Sobre Nosotros</h2>
+
+            <p>
+                SOLO FRENOS EL MECHUDO es un taller mecánico especializado exclusivamente en sistemas de frenado.
+                Con más de 40 años en el mercado, somos referencia en mantenimiento y reparación.
+            </p>
+
+            <div class="features">
+                <div>
+                    <h4>40+ Años</h4>
+                    <p>Experiencia</p>
+                </div>
+                <div>
+                    <h4>Equipo</h4>
+                    <p>Certificado</p>
+                </div>
+                <div>
+                    <h4>Servicio</h4>
+                    <p>Rápido</p>
+                </div>
+                <div>
+                    <h4>Garantía</h4>
+                    <p>Total</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="nosotros-img">
+            <img src="../assets/img/hero.png">
+        </div>
+
+    </div>
+</section>
